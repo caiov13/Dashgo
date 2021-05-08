9f66fc6edf09ab140b66e7a7be579698aeab30d9
Caio Vinicius <aio_vinicius11111@hotmail.com>
2021-05-08 18:40:35 -0300

    Form and Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex 
+    w="100vw" 
+    h="100vh" 
+    alignItems="center" 
+    justifyContent="center"
+    >
+      <Flex
+      as="form"
+      w="100%"
+      maxWidth={360}
+      bg="gray.800"
+      p="8" // 32px / 2rem
+      borderRadius="8px"
+      flexDirection="column"
+      >
+        <Stack spacing="4">
+
+          <Input name="email" type="email" label="E-mail"/>
+          <Input name="password" type="password" label="Senha"/>
+
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        
+      </Flex>
+    </Flex>
   )
 }
