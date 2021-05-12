60facdcb78ef6bde46aa4d82dd8f83ca74dfee98
Caio Vinicius <aio_vinicius11111@hotmail.com>
2021-05-12 18:15:16 -0300

    Add form validation :tada:

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,13 +1,16 @@
-import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps, FormErrorMessage } from "@chakra-ui/react";
+import { FieldError } from 'react-hook-form'
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps  extends ChakraInputProps {
     name: string;
     label?: string;
+    error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }, ref) => {
     return(
-        <FormControl>
+        <FormControl isInvalid={!!error}>
          { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
           <ChakraInput 
@@ -20,9 +23,16 @@ export function Input({ name, label, ...rest }: InputProps) {
             bgcolor:'gray.900'
           }}
           size="lg" // 48px 
+          ref={ref}
           {...rest}
           />
           
+          {!!error && (
+            <FormErrorMessage>{error.message}</FormErrorMessage>
+          )}
+
           </FormControl>
     );
 }
+
+export const Input = forwardRef(InputBase);
